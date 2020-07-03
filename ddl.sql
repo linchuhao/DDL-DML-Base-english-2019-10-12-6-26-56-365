789291d74950553ca155b1f8ceb3e37973f33b24
linchuhao <370356141@qq.com>
2020-07-03 17:11:00 +0800

    SQL homework.

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use student;
 -- Create a database
-
+create database student;
 -- Create the database of the designated character set
-
+create database student character set utf8mb4;
 -- Display the creation information fo the database
-
+show create database student;
 -- Revise the codes of the database
-
+alter database student character set utf8;
 -- Delete a database
-
+drop database student;
 -- **Table level**
 -- Revise table name
-
+alter table student rename students;
 -- Revise the field's data type
-
+alter table student modify name varchar(18);
 -- Revise field name
-
+alter table student change sex gender varchar(18);
 -- Add field
-
+alter table student add column phone varchar(11);
 -- Delete field
-
+alter table student drop column email;
 -- Revise the table's storage engine
-
+alter table student engine = MyISAM;
 -- Delete the table's foreign key restriant
-
+alter table student drop foreign key foreign_key_name;
 -- Delete a table
+drop table score;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
 -- Insert record
-
+insert into student values('0756','Lester',18,'male'),('0755','Eva',20,'female');
 -- Revise record
-
+update student set age =22 where id = '0756';
+update student set age =21 where id = '0755';
 --  Delete record
-
+delete from student where id ='0756';
 -- Search record
+select * from student;
